1d943a6c8239eb5e30044c8026304dc5bbc58558
sakilk130 <sakilk130@gmail.com>
2022-08-25 23:47:44 +0600

    feat: :sparkles: create new document using firebase

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -6,7 +6,10 @@ import {
   DialogHeader,
   Input,
 } from "@material-tailwind/react";
-import React from "react";
+import firebase from "firebase";
+import { useSession } from "next-auth/client";
+import { useState } from "react";
+import db from "../../db/firebase";
 
 interface IModal {
   handleOpen: any;
@@ -15,11 +18,36 @@ interface IModal {
 }
 
 const Modal = ({ handleOpen, isOpen, size }: IModal) => {
+  const [title, setTitle] = useState("");
+  const [session]: any = useSession();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleCreateNewDocs = () => {
+    if (title !== "") {
+      setIsLoading(true);
+      db.collection("userDocs")
+        .doc(session?.user?.email)
+        .collection("docs")
+        .add({
+          fileName: title,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+          lastUpdated: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+      handleOpen(null);
+      setTitle("");
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Dialog handler={handleOpen} open={isOpen} size={size}>
       <DialogHeader>Create new Document</DialogHeader>
       <DialogBody divider>
-        <Input label="Enter name of document" />
+        <Input
+          label="Enter name of document"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
       </DialogBody>
       <DialogFooter>
         <Button
@@ -30,12 +58,8 @@ const Modal = ({ handleOpen, isOpen, size }: IModal) => {
         >
           <span>Cancel</span>
         </Button>
-        <Button
-          variant="gradient"
-          color="green"
-          onClick={() => handleOpen(null)}
-        >
-          <span>Confirm</span>
+        <Button variant="gradient" color="green" onClick={handleCreateNewDocs}>
+          <span>{isLoading ? "Creating..." : "Confirm"}</span>
         </Button>
       </DialogFooter>
     </Dialog>
